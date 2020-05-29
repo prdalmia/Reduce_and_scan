30652640b52baafc3561079f3b077c235f013a88
prdalmia <pdalmia@wisc.edu>
2020-05-29 18:30:21 -0500

    message

--- reduce_with_barrier.cu
+++ reduce_with_barrier.cu
@@ -204,7 +204,9 @@ const int perSM_blockID = (blockIdx.x / numBlocksAtBarr);
 
 int numTBs_perSM = (int)ceil((float)gridDim.x / numBlocksAtBarr);
 
-
+if(isMasterThread && smID == 0 && blockIdx.x == 0){
+    printf("Launching barrier\n");
+}
 joinBarrier_helperSRB(global_sense, perSMsense, done, global_count, local_count, last_block,
 numBlocksAtBarr, smID, perSM_blockID, numTBs_perSM,
 isMasterThread);
