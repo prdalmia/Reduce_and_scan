1e290ea38a409da1d2fd4b71e35358f6b5a98ac5
prdalmia <pdalmia@wisc.edu>
2020-05-30 18:48:05 -0500

    message

--- scan_with_barrier_base.cu
+++ scan_with_barrier_base.cu
@@ -33,7 +33,7 @@ __global__ void hillis_steele(float* g_odata, float* lasts,  float* g_idata, uns
         s[tid] = 0.f;
     } else {
         s[tid] = g_idata[index - 1];
-        if(a <n )
+        if(a < n)
         printf("g_idata is %f at index %d\n", g_idata[index-1], index-1);
     }
     
