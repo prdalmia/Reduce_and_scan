d5590b7c20d9075db3bd79e14d7cae70ce847128
prdalmia <pdalmia@wisc.edu>
2020-05-29 20:44:01 -0500

    message

--- reduce_with_barrier.cu
+++ reduce_with_barrier.cu
@@ -29,7 +29,7 @@ while (*global_sense != *sense)
 {
 if (isMasterThread)
 {
-// printf("Global sense hili\n");
+ printf("Global sense hili\n");
 /*
 For the tree barrier we expect only 1 TB from each SM to enter the
 global barrier.  Since we are assuming an equal amount of work for all
