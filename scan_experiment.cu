2fd68da638247de9b6e1a0e3eb40c256d8f9d112
prdalmia <pdalmia@wisc.edu>
2020-05-30 13:04:41 -0500

    message

--- scan_experiment.cu
+++ scan_experiment.cu
@@ -45,7 +45,7 @@ __global__ void hillis_steele(float* g_odata, float* lasts,  float* g_idata, uns
     if (write_lasts && threadIdx.x == 0) {
         unsigned int block_end = blockIdx.x * blockDim.x + blockDim.x - 1;
         lasts[blockIdx.x] = s[pout * blockDim.x + blockDim.x - 1] + g_idata[block_end];
-        printf("Lasts is %d\n", lasts[blockIdx.x]);
+        printf("Lasts is %f\n", lasts[blockIdx.x]);
     }
 }
 
