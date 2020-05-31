f8c62a84d969a4f24542a07a9ff893c6766749aa
prdalmia <pdalmia@wisc.edu>
2020-05-30 20:13:03 -0500

    message

--- scan_with_barrier.cu
+++ scan_with_barrier.cu
@@ -27,7 +27,7 @@ __threadfence();
 // atomicInc effectively adds 1 to atomic for each TB that's part of the
 // global barrier.
 atomicInc(globalBarr, 0x7FFFFFFF);
-printf("globalBarr is %d", *globalBarr)
+printf("globalBarr is %d", *globalBarr);
 }
 __syncthreads();
 
