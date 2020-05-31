ee01971db32bacb7a02dda1b4d5739a8d920a0dc
prdalmia <pdalmia@wisc.edu>
2020-05-30 19:12:22 -0500

    message

--- scan_with_barrier_base.cu
+++ scan_with_barrier_base.cu
@@ -71,6 +71,7 @@ __global__ void hillis_steele(float* g_odata, float* lasts,  float* g_idata, uns
       g_odata = lasts;
       write_p = false;
       a = (n + blockDim.x - 1) / blockDim.x;
+      __threadfence();
     }
     __syncthreads();
 }
