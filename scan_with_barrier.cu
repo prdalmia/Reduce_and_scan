0f0d6d900650f52dbb5f9cb72de36ed867c28b5e
prdalmia <pdalmia@wisc.edu>
2020-05-30 22:17:43 -0500

    message

--- scan_with_barrier.cu
+++ scan_with_barrier.cu
@@ -47,6 +47,7 @@ if (atomicCAS(globalBarr, numBarr, 0) == numBarr) {
 // atomicCAS acts as a load acquire, need TF to enforce ordering
 __threadfence();
 *global_sense = *sense;
+__threadfence();
  printf("Setting global sense = sense \n");
 }
 else { // increase backoff to avoid repeatedly hammering global barrier
