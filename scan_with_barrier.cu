437f6814c9ec5f7712417f6fbafee3fb621b0d73
prdalmia <pdalmia@wisc.edu>
2020-05-30 23:19:57 -0500

    message

--- scan_with_barrier.cu
+++ scan_with_barrier.cu
@@ -58,8 +58,8 @@ __syncthreads();
 
 // do exponential backoff to reduce the number of times we pound the global
 // barrier
-if (*global_sense != *sense) {
-for (int i = 0; i < backoff; ++i) { ; }
+//if (*global_sense != *sense) {
+//for (int i = 0; i < backoff; ++i) { ; }
 //__syncthreads();
 }
 }
