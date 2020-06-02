080abe194b1e8dabe7c1af66831016f676c133e0
prdalmia <pdalmia@wisc.edu>
2020-06-01 19:26:04 -0500

    message

--- scan_with_barrier.cu
+++ scan_with_barrier.cu
@@ -61,7 +61,7 @@ else { // increase backoff to avoid repeatedly hammering global barrier
    // if (*global_sense != *sense) {
     //for (int i = 0; i < backoff; ++i) { ; }
    // __syncthreads();
-  //  }
+  }
 }
 
 inline __device__ void cudaBarrierAtomicSRB(unsigned int * barrierBuffers,
