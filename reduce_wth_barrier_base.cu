b24b78390b682e7cf3eae9b8ae983d49255e2071
prdalmia <pdalmia@wisc.edu>
2020-06-05 10:08:02 -0500

    message

--- reduce_wth_barrier_base.cu
+++ reduce_wth_barrier_base.cu
@@ -76,7 +76,7 @@ __host__ int reduce(const int* arr, unsigned int N, unsigned int threads_per_blo
     float ms;
     cudaEventElapsedTime(&ms, start, stop);
     std::cout << "time cuda only(ms) " << ms << std::endl;
-    int sum = a[0];
+    int sum = output;
 
     cudaFree(a);
     cudaFree(b);
