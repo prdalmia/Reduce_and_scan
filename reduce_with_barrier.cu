84de4f40ef3c17d6de2d4d58e28a2da4e1034fbe
prdalmia <pdalmia@wisc.edu>
2021-04-08 15:28:41 -0500

    message

--- reduce_with_barrier.cu
+++ reduce_with_barrier.cu
@@ -80,7 +80,7 @@ inline __device__ void cudaBarrierAtomicSubSRB(unsigned int * globalBarr,
   // atomicInc effectively adds 1 to atomic for each TB that's part of the
   // global barrier.
   atomicInc(globalBarr, 0x7FFFFFFF);
-  //printf("Global barr is %d\n", *globalBarr);
+  printf("Global barr is %d and numBarr is %d\n", *globalBarr, numBarr);
   }
   __syncthreads();
   
@@ -88,7 +88,6 @@ inline __device__ void cudaBarrierAtomicSubSRB(unsigned int * globalBarr,
   {
   if (isMasterThread)
   {
-  printf("Global sense hili\n");
   /*
   For the tree barrier we expect only 1 TB from each SM to enter the
   global barrier.  Since we are assuming an equal amount of work for all
